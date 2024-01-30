be20d5949c87e27e8c3faa45b69904f1767ae631
İkanaide <143414698+kosero@users.noreply.github.com>
2024-01-30 09:26:41 +0300

    Rei

--- index.js
+++ index.js
@@ -18,51 +18,64 @@ function showTime() {
 showTime();
 
 function aramaYap() {
-	let inputMetni = document.getElementById("display").value.trim();
-	
-	if (inputMetni === "") {
-	  alert("Boş olmaz.");
-	  return;
-	}
-  
-	let kelimeler = inputMetni.split(" ");
-	let ilkKelime = kelimeler[0];
-	let aramaURL = "";
-  
-	switch (ilkKelime) {
-	  case "!yt":
-		let ytGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://www.youtube.com/results?search_query=" + encodeURIComponent(ytGeriKalanMetin);
-		break;
-	  
-	  case "!wk":
-		let wkGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://tr.wikipedia.org/wiki/" + encodeURIComponent(wkGeriKalanMetin);
-		break;
-  
-	  case "!pin":
-		let pinGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://tr.pinterest.com/search/pins/?q=" + encodeURIComponent(pinGeriKalanMetin);
-		break;
-  
-	  case "!git":
-		let gitGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://github.com/search?q=" + encodeURIComponent(gitGeriKalanMetin);
-		break;
-  
-	  case "!pixiv":
-		let pixivGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://www.pixiv.net/en/tags/" + encodeURIComponent(pixivGeriKalanMetin);
-		break;
-  
-	  case "!anx":
-		let anxGeriKalanMetin = kelimeler.slice(1).join(" ");
-		aramaURL = "https://animecix.net/search?query=" + encodeURIComponent(anxGeriKalanMetin);
-		break;
-  
-	  default:
-		aramaURL = "https://www.google.com/search?q=" + encodeURIComponent(inputMetni);
-		break;
-	}
-	window.location.href = aramaURL;
+    let inputMetni = document.getElementById("display").value.trim();
+
+    if (inputMetni === "") {
+        alert("Boş olmaz.");
+        return;
+    }
+
+    let kelimeler = inputMetni.split(" ");
+    let ilkKelime = kelimeler[0];
+    let aramaURL = "";
+
+    switch (ilkKelime) {
+        case "!yt":
+            {
+                let ytGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://www.youtube.com/results?search_query=" + encodeURIComponent(ytGeriKalanMetin);
+                break;
+            }
+
+        case "!wk":
+            {
+                let wkGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://tr.wikipedia.org/wiki/" + encodeURIComponent(wkGeriKalanMetin);
+                break;
+            }
+
+        case "!pin":
+            {
+                let pinGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://tr.pinterest.com/search/pins/?q=" + encodeURIComponent(pinGeriKalanMetin);
+                break;
+            }
+
+        case "!git":
+            {
+                let gitGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://github.com/search?q=" + encodeURIComponent(gitGeriKalanMetin);
+                break;
+            }
+
+        case "!pixiv":
+            {
+                let pixivGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://www.pixiv.net/en/tags/" + encodeURIComponent(pixivGeriKalanMetin);
+                break;
+            }
+
+        case "!anx":
+            {
+                let anxGeriKalanMetin = kelimeler.slice(1).join(" ");
+                aramaURL = "https://animecix.net/search?query=" + encodeURIComponent(anxGeriKalanMetin);
+                break;
+            }
+
+        default:
+            aramaURL = "https://www.google.com/search?q=" + encodeURIComponent(inputMetni);
+            break;
+    }
+
+    window.location.href = aramaURL;
 }
